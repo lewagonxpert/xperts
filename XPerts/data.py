d7b898a20e02c7d20aa494cbdca589d02abd2544
seung ah pyun <seungahpyun@icloud.com>
2022-06-01 14:08:35 +0200

    data

--- XPerts/data.py
+++ XPerts/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from google.cloud import storage
 import tensorflow as tf
-from XPerts.params import BUCKET_NAME, BUCKET_TRAIN_X_PATH #BUCKET_TRAIN_y_PATH
+from XPerts.params import BUCKET_NAME, BUCKET_TRAIN_X_PATH,BUCKET_TRAIN_y_PATH
 from os.path import dirname, abspath, join
 from dotenv import load_dotenv
 import os
@@ -17,16 +17,22 @@ def get_X_from_gcp():
     storage_client = storage.Client.from_service_account_json(os.getenv("gcp_json_path"))
     bucket = storage_client.bucket(BUCKET_NAME)
 
-    num = [*range(1,399)]
-    image = []
+    num = [*range(1,50)]
 
     for i in num:
         blob = bucket.blob(f'{BUCKET_TRAIN_X_PATH}/{i}.jpg')
-        img = blob.download_to_filename(f'XPerts/data/{i}.jpg')
-        #image.append(tf.expand_dims(tf.keras.preprocessing.image.img_to_array(img), axis=0))
+        img = blob.download_to_filename(f'/XPerts/data/{i}.jpg')
 
-    #X = tf.concat(image, 0)
 
+def X_to_tensor():
+    num = [*range(1,50)]
+    image = []
+
+    for i in num:
+        img = tf.keras.utils.load_img(f"./XPerts/data/{i}.jpg")
+        image.append(tf.expand_dims(tf.keras.preprocessing.image.img_to_array(img), axis=0))
+    X = tf.concat(image, 0)
+    print(type(X))
 
 
 # def get_y_from_gcp():
@@ -41,5 +47,5 @@ def get_X_from_gcp():
 #     return y_cat
 
 if __name__ == '__main__':
-    X = get_X_from_gcp()
+    X = X_to_tensor()
 #     y_cat = get_y_from_gcp()
