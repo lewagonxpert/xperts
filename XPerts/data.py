9642ce1f56b81cab8452ffd1713d8d745e3e8069
seung ah pyun <seungahpyun@icloud.com>
2022-06-01 14:54:55 +0200

    data

--- XPerts/data.py
+++ XPerts/data.py
@@ -11,41 +11,49 @@ import os
 
 env_path = join(dirname(abspath(__file__)),'.env') # ../.env
 load_dotenv(env_path)
+csv_path = './XPerts/data/data.csv'
+data_path = './XPerts/data'
 
-#get data from GCP
+
 def get_X_from_gcp():
     storage_client = storage.Client.from_service_account_json(os.getenv("gcp_json_path"))
     bucket = storage_client.bucket(BUCKET_NAME)
 
-    num = [*range(1,50)]
+    num = [*range(1,590)]
 
     for i in num:
         blob = bucket.blob(f'{BUCKET_TRAIN_X_PATH}/{i}.jpg')
-        img = blob.download_to_filename(f'/XPerts/data/{i}.jpg')
+        img = blob.download_to_filename(f'{data_path}/{i}.jpg')
 
 
 def X_to_tensor():
-    num = [*range(1,50)]
+    num = [*range(1,2)]
     image = []
 
     for i in num:
-        img = tf.keras.utils.load_img(f"./XPerts/data/{i}.jpg")
+        img = tf.keras.utils.load_img(f"{data_path}/{i}.jpg")
         image.append(tf.expand_dims(tf.keras.preprocessing.image.img_to_array(img), axis=0))
     X = tf.concat(image, 0)
-    print(type(X))
+    return X
 
 
-# def get_y_from_gcp():
-#     client = storage.Client()
-#     data = pd.read_csv('BUCKET_TRAIN_y_PATH')
-#     data=data.drop(columns='Unnamed: 2')
-#     data['Nu_Teeth']=data['Nu_Teeth'].replace(np.nan,'-1')
-#     data['Nu_Teeth']=data['Nu_Teeth'].replace('B','-1')
-#     y = data['Nu_Teeth']
-#     y_cat = tf.keras.utilsto_categorical(y, num_classes=44)
-#     y_cat = tf.convert_to_tensor(y_cat)
-#     return y_cat
+def get_y_from_gcp():
+    storage_client = storage.Client.from_service_account_json(os.getenv("gcp_json_path"))
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(BUCKET_TRAIN_y_PATH)
+    data = blob.download_to_filename(csv_path)
+
+def y_to_tensor():
+    data = pd.read_csv(csv_path)
+    data = data.drop(columns='Unnamed: 2')
+    data['Nu_Teeth'] = data['Nu_Teeth'].replace(np.nan,'-1')
+    data['Nu_Teeth'] = data['Nu_Teeth'].replace('B','-1')
+    y = data['Nu_Teeth']
+    y_cat = tf.keras.utils.to_categorical(y, num_classes=44)
+    y_cat = tf.convert_to_tensor(y_cat)
+    return y_cat
 
 if __name__ == '__main__':
     X = X_to_tensor()
-#     y_cat = get_y_from_gcp()
+    y_cat = y_to_tensor()
+    print(y_cat)
