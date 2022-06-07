816375253b91f86646afd62f3abc7ea0ed78e9dc
seung ah pyun <seungahpyun@icloud.com>
2022-06-07 10:11:11 +0200

    predict

--- XPerts/predict.py
+++ XPerts/predict.py
@@ -1,15 +1,14 @@
-import joblib
 import os
 from google.cloud import storage
 from keras.models import load_model
-
+import h5py
 from XPerts.data import get_X_from_gcp, get_y_from_gcp,X_to_tensor,y_to_tensor
 from XPerts.params import BUCKET_NAME, BUCKET_TRAIN_X_PATH,BUCKET_TRAIN_y_PATH
 
+PATH_TO_LOCAL_MODEL = 'model.h5'
 
-
-def get_model(path_to_joblib):
-    model = load_model(path_to_joblib)
+def get_model(path_to_h5):
+    model = load_model(path_to_h5)
     return model
 
 def download_model(bucket=BUCKET_NAME, rm=True):
@@ -18,7 +17,8 @@ def download_model(bucket=BUCKET_NAME, rm=True):
     storage_location = f'model/xperts/v1/{local_model_name}'
     blob = client.blob(storage_location)
     blob.download_to_filename('model.h5')
-    model = joblib.load('model.h5')
+    model_gcs = h5py.File('model.h5', 'r')
+    model = load_model(model_gcs)
     if rm:
         os.remove('model.h5')
     return model
