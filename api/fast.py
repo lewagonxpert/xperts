7dcc6c4e0f3ec07a0debc22ba79f1edd6b1e84e6
seung ah pyun <seungahpyun@icloud.com>
2022-06-08 21:59:17 +0200

    api

--- api/fast.py
+++ api/fast.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from google.cloud import storage
 import io
 import numpy as np
 from keras.models import load_model
 from PIL import Image, ImageDraw
 from fastapi import FastAPI, File, UploadFile, Form, Request
 import tensorflow as tf
+from XPerts.params import BUCKET_NAME
+import h5py
 
 app = FastAPI()
 
@@ -37,7 +40,14 @@ def run_model(bytes):
     image_array = [np.expand_dims(np.asarray(image),axis=0)]
     X_p=tf.concat(image_array, 0)
     # Load our model
-    model = load_model('model.h5')
+    client = storage.Client().bucket(BUCKET_NAME)
+    local_model_name = 'model.h5'
+    storage_location = f'model/xperts/v1/{local_model_name}'
+    blob = client.blob(storage_location)
+    model_gcs = blob.download_as_bytes()
+    f = io.BytesIO(model_gcs)
+    h = h5py.File(f,'r')
+    model = load_model(h)
     # Get results
     y_p=model.predict(X_p)
     y_p=y_p.reshape(5,3)
